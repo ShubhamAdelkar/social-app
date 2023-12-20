079a37209ec257b52ab126f42e1ff1f08f413287
Shubham Adelkar <adelkarshubham47@gmail.com>
2023-12-20 15:41:07 +0530

    bottombar layout

--- src/components/ui/shared/Bottombar.tsx
+++ src/components/ui/shared/Bottombar.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { bottombarLinks } from "@/constants";
+import { Link, useLocation } from "react-router-dom";
 
 const Bottombar = () => {
-  return <div>Bottombar</div>;
+  const { pathname } = useLocation();
+
+  return (
+    <section className="bottom-bar">
+      {bottombarLinks.map((link) => {
+        const isActive = pathname === link.route;
+        return (
+          <Link
+            to={link.route}
+            key={link.route}
+            className={` ${
+              isActive && "bg-primary-500 rounded-[12px]"
+            } flex-center flex-col gap-1 p-2 px-4 transition py-2`}
+          >
+            <img
+              src={link.imgURL}
+              alt={link.label}
+              width={24}
+              height={24}
+              className={`${isActive && "invert-white"} `}
+            />
+            <p className="tiny-medium">{link.label}</p>
+          </Link>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Bottombar;
