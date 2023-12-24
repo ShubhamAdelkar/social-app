fc7a8e23765ccc51f59f741f6172916ef408a3bb
Shubham Adelkar <adelkarshubham47@gmail.com>
2023-12-24 11:34:19 +0530

    comment in home

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home = () => {
   const {
     data: posts,
     isPending: isPostLoading,
-    isError: isErrorPosts,
+    // isError: isErrorPosts,
   } = useGetRecentPosts();
   return (
     <div className="flex flex-1">
