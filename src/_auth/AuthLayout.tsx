30d785926a0be879774efc9e2346e30dbdee0fb6
Shubham Adelkar <adelkarshubham47@gmail.com>
2023-12-24 12:24:13 +0530

    change in authlayout

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -16,7 +16,7 @@ const AuthLayout = () => {
           </section>
 
           <img
-            src="/assets/images/side-img.svg"
+            src="/assets/images/side-img.jpeg"
             alt="logo"
             className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
           />
