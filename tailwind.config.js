f5517ff9abdcad52bb9da22adc70cd120e9a276b
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-03-17 11:39:44 +0530

    chnage in css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,77 +2,74 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  darkMode: ['class'],
+  darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   theme: {
     container: {
       center: true,
-      padding: '2rem',
+      padding: "2rem",
       screens: {
-        'sm': '640px',
+        xs: "480px",
+        // => @media (min-width: 480px) { ... }
+
+        sm: "640px",
         // => @media (min-width: 640px) { ... }
 
-        'md': '768px',
+        md: "768px",
         // => @media (min-width: 768px) { ... }
 
-        'lg': '1024px',
+        lg: "1024px",
         // => @media (min-width: 1024px) { ... }
 
-        'xl': '1280px',
+        xl: "1280px",
         // => @media (min-width: 1280px) { ... }
 
-        '2xl': '1400px',
-
+        "2xl": "1400px",
       },
     },
     extend: {
       colors: {
-        'primary-500': '#F78F8F',
-        'primary-600': '#5D5FEF',
-        'secondary-500': '#FFB620',
-        'off-white': '#D0DFFF',
-        'red': '#FF5A5A',
-        'dark-1': '#000000',
-        'dark-2': '#09090A',
-        'dark-3': '#101012',
-        'dark-4': '#1F1F22',
-        'light-1': '#FFFFFF',
-        'light-2': '#EFEFEF',
-        'light-3': '#DCD5F2',
-        'light-4': '#5C5C7B',
-      },
-      screens: {
-        'xs': '480px',
-
+        "primary-500": "#F78F8F",
+        "primary-600": "#5D5FEF",
+        "secondary-500": "#FFB620",
+        "off-white": "#D0DFFF",
+        red: "#FF5A5A",
+        "dark-1": "#000000",
+        "dark-2": "#09090A",
+        "dark-3": "#101012",
+        "dark-4": "#1F1F22",
+        "light-1": "#FFFFFF",
+        "light-2": "#EFEFEF",
+        "light-3": "#DCD5F2",
+        "light-4": "#5C5C7B",
       },
       width: {
-        '420': '420px',
-        '465': '465px',
+        420: "420px",
+        465: "465px",
       },
       fontFamily: {
-        inter: ['Inter', 'sans-serif'],
-
+        inter: ["Inter", "sans-serif"],
       },
       keyframes: {
-        'accordion-down': {
+        "accordion-down": {
           from: { height: 0 },
-          to: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: "var(--radix-accordion-content-height)" },
         },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
       },
       animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require("tailwindcss-animate")],
 };
