a2d50638c8a9f3f0c756efcd94b2a3644519f7b2
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-03-17 11:39:56 +0530

    chnage in UI

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,5 +1,3 @@
-// import Loader from "@/components/ui/shared/Loader";
-import Loader from "@/components/ui/shared/Loader";
 import PostCard from "@/components/ui/shared/PostCard";
 import { useGetRecentPosts } from "@/lib/react-query/queriesAndMutations";
 import { Models } from "appwrite";
@@ -18,24 +16,22 @@ const Home = () => {
           <h3 className="h3-bold md:h2-bold text-left w-full">Home Feed</h3>
           <iframe
             className="md:w-[86%]"
-            src="https://open.spotify.com/embed/track/3xKsf9qdS1CyvXSMEid6g8?utm_source=generator&theme=0"
+            src="https://open.spotify.com/embed/track/7MXVkk9YMctZqd1Srtv4MB?utm_source=generator"
             width="100%"
-            height="352"
+            height="152"
             allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
             loading="lazy"
           ></iframe>
           {isPostLoading && !posts ? (
             <ul className="flex flex-col flex-1 gap-9 w-full">
-              {Array.from({ length: 5 }).map((_, index) => (
+              {Array.from({ length: 12 }).map((_, index) => (
                 <li key={index} className="flex justify-center w-full">
-                  <div className="placeholder-post-card">
-                    <Loader />
-                  </div>
+                  <div className="placeholder-post-card"></div>
                 </li>
               ))}
             </ul>
           ) : (
-            <ul className="flex flex-col flex-1 gap-9 w-full">
+            <ul className="flex flex-col flex-1 gap-2 w-full">
               {posts?.documents.map((post: Models.Document) => (
                 <li key={post.id} className="flex justify-center w-full">
                   <PostCard post={post} key={post.caption} />
