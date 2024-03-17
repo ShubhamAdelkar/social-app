a423e79ef5bc62875388806a1db30a269f6578a5
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-03-17 11:40:28 +0530

    Add user name and profile photo

--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -1,13 +1,26 @@
+import { useUserContext } from "@/context/AuthContext";
 
 const Profile = () => {
+  const { user } = useUserContext();
+  console.log(user);
   return (
     <div className="explore-container mb-14 lg:mb-0">
-      <div className="flex-between w-full max-w-5xl mt-8 mb-7 lg:mt-14">
-        <h2 className="body-bold md:h3-bold">Your Profile</h2>
+      <div className="flex-col flex-between w-full max-w-5xl mt-8 mb-7 lg:mt-14">
+        <div className="flex flex-col gap-3 items-center">
+          <img
+            src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
+            alt="profile"
+            className="rounded-full h-[100px] w-[100px]"
+          />
+          <div className="flex flex-col items-center">
+            <p className="body-bold">{user.name}</p>
+            <p className="small-regular text-light-3">@{user.username}</p>
+          </div>
+        </div>
       </div>
-      <p className="text-light-3">This is a beta version</p>
+      {/* <p className="text-light-3">This is a beta version</p> */}
     </div>
   );
-}
+};
 
-export default Profile
+export default Profile;
